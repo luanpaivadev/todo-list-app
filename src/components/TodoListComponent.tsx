3f0a68c66c616ef8522e2c0ba3877e5a1816202b
Luan Paiva <luanpaivadev@gmail.com>
2022-10-03 18:55:47 -0300

    Adicionado módulo SweetAlert2 e implementado o botão de exclusão de tarefa

--- src/components/TodoListComponent.tsx
+++ src/components/TodoListComponent.tsx
@@ -1,9 +1,10 @@
 import { Box, List } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 import './TodoListComponent.css'
 
-declare interface TodoListComponentProps {
+interface TodoListComponentProps {
     title: string
     data?: Task[]
 }
@@ -11,13 +12,12 @@ declare interface TodoListComponentProps {
 interface Task {
     id: number
     description: string
-    completed: boolean
+    completed?: boolean
 }
-
 
 const TodoListComponent: React.FC<TodoListComponentProps> = (props) => {
 
-    const taskInit: Task = { id: 0, description: '', completed: false }
+    const taskInit: Task = { id: 0, description: '' }
 
     const completed = 'list-group-item list-group-item-action list-group-item-success'
     const noCompleted = 'list-group-item list-group-item-action'
@@ -26,7 +26,7 @@ const TodoListComponent: React.FC<TodoListComponentProps> = (props) => {
     const [task, setTask] = useState(taskInit)
     const [count, setCount] = useState(1)
 
-    function setDone(task: Task) {
+    function setDone(task: Task): void {
         if (!task.completed) {
             task.completed = true
         } else {
@@ -35,12 +35,33 @@ const TodoListComponent: React.FC<TodoListComponentProps> = (props) => {
         setTasks(tasks.map(tarefa => task.id === tarefa.id ? task : tarefa))
     }
 
-    function addTask() {
+    function addTask(): void {
         if (task && task.description && task.description.trim().length > 0) {
             setTasks([...tasks, task])
             setCount(count + 1)
             setTask(taskInit)
         }
+    }
+
+    function deleteTask(task: Task) {
+        Swal.fire({
+            title: 'Deseja excluir a tarefa selecionada?',
+            text: "Não será possível reverter isto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                setTasks(tasks.filter(t => t.id != task.id))
+                Swal.fire(
+                    'Deletado!',
+                    'Tarefa deletada com sucesso!',
+                    'success'
+                )
+            }
+        })
     }
 
     return (
@@ -60,9 +81,9 @@ const TodoListComponent: React.FC<TodoListComponentProps> = (props) => {
                                 label="Descrição da tarefa"
                                 variant="standard"
                                 value={task?.description}
-                                onChange={event => setTask({ id: count, description: event.target.value, completed: false })}
+                                onChange={event => setTask({ id: count, description: event.target.value })}
                                 onKeyDown={event => event.key === 'Enter' ? addTask() : ''} />
-                            <div id="emailHelp" className="form-text">Precione ENTER para adicionar a tarefa na lista.</div>
+                            <div id="emailHelp" className="form-text">Precione ENTER para adicionar uma tarefa na lista.</div>
                         </div>
                     </Box>
                 </div>
@@ -80,17 +101,29 @@ const TodoListComponent: React.FC<TodoListComponentProps> = (props) => {
                 <ul className='list-group list-group-flush'>
                     {
                         tasks.map(task => (
-                            <li key={task.id} className={task.completed ? completed : noCompleted}
-                                onClick={() => setDone(task)}>
+                            <li key={task.id} className={task.completed ? completed : noCompleted}>
                                 <div className='row align-items-center'>
-                                    <div className='col-11'>
+                                    <div className='col-10' onClick={() => setDone(task)}>
                                         <span className='align-middle'> <strong>#{task.id}</strong> - {task.description}</span>
                                     </div>
                                     <div className='col-1 text-center'>
-                                        <span className="material-symbols-outlined align-middle">
-                                            {task.completed ? 'check' : ''}
-                                        </span>
+                                        {
+                                            task.completed &&
+                                            <span className="material-symbols-outlined align-middle">
+                                                check
+                                            </span>
+                                        }
                                     </div>
+                                    {
+                                        task.completed &&
+                                        <div className='col-1'>
+                                            <button id='btn-delete' onClick={() => deleteTask(task)}>
+                                                <span className="material-symbols-outlined align-middle">
+                                                    delete
+                                                </span>
+                                            </button>
+                                        </div>
+                                    }
                                 </div>
                             </li>
                         ))
